b437cce83d8b29e6bbba24dcfbc42bc84d48a54e
Aryan Rajnandan <z5359972@student.unsw.edu.au>
2026-04-18 21:47:47 +1000

    cards

--- Cards/Main.java
+++ Cards/Main.java
@@ -22,7 +22,7 @@ enum Rank {
     }
 }
 
-public class Card {
+class Card {
   private Suit suit;
   private Rank rank;
 
@@ -46,20 +46,72 @@ public class Card {
 
 }
 
-public class Deck {
-  List<Card> cards;
+class Deck {
+  private List<Card> cards;
 
   public Deck() {
-    this.cards = new ArrayList<Card>();
-    for s in suit:
+    cards = new ArrayList<Card>();
+    for (Suit suit : Suit.values()) {
+      for (Rank rank : Rank.values()) {
+        cards.add(new Card(suit, rank));
+      }
+    }
   }
 
-  public
+  public void Shuffle() {
+    Collections.shuffle(cards);
+  }
+
+  public List<Card> getCards() {
+    return cards;
+  }
+
+  public Card draw() {
+    if (cards.isEmpty()) {
+      throw new IllegalStateException("Deck is empty");
+    }
+    // top of this list
+    return cards.remove(0);
+  }
+}
+
+class Hand {
+  private List<Card> cards;
+  private Deck deck;
+
+  public Hand(Deck deck) {
+    this.cards = new ArrayList<Card>();
+    this.deck = deck;
+  }
+
+  public void addCard() {
+    Card card = deck.draw();
+    cards.add(card);
+  }
+
+  public List<Card> getCards() {
+    return cards;
+  }
 }
 
 public class Main {
-  public static void main(String[] args) {
-    Deck deck = new Deck();
-  }
-  
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.Shuffle();
+
+        Hand hand = new Hand(deck);
+
+        for (int i = 0; i < 5; i++) {
+            hand.addCard();
+        }
+
+
+        System.out.println("Remaining deck:");
+        for (Card card : deck.getCards()) {
+            System.out.println(card.getRank() + " of " + card.getSuit());
+        }
+    }
 }
+
+
+  
